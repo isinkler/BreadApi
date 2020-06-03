4364be7fddcf6d777cd9ef39541c5ed09ac2c919
isinkler <isinkler@me.com>
2020-06-03 14:54:11 +0200

    Minor Docker environment fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@
 set -e
 run_cmd="dotnet run -p /app/Bread.WebApi --launch-profile Bread-Docker"
 
-until dotnet-ef -s /app/Bread.WebApi -p /app/Bread.Data -v --environment Docker database update; do
->&2 echo "SQL Server is starting up lol"
+until dotnet ef -s /app/Bread.WebApi -p /app/Bread.Data -v database update; do
+>&2 echo "SQL Server is starting up..."
 sleep 1
 done
 
->&2 echo "SQL Server is up - executing command"
+>&2 echo "SQL Server is up!"
+
+>&2 echo "Starting WebApi..."
 exec $run_cmd
