1cffb8e147f7f723ea5030b8ffde829a389906eb
isinkler <isinkler@me.com>
2020-07-15 13:16:41 +0200

    Add cities migration

--- Bread.Data/Migrations/20200715111259_AddCitiesTable.Designer.cs
+++ Bread.Data/Migrations/20200715111259_AddCitiesTable.Designer.cs
@@ -4,14 +4,16 @@ using Bread.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bread.Data.Migrations
 {
     [DbContext(typeof(BreadDbContext))]
-    partial class BreadDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200715111259_AddCitiesTable")]
+    partial class AddCitiesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
